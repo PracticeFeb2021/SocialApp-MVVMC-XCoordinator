03727f639dae69871ceb6e9caf5c51687d14d202
Oleksandr Bretsko <o.bretsko@selectoglobal.com>
2021-02-26 13:41:33 +0200

    Initial Commit

---
+++ SocialApp - MVVMC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialApp - MVVMC
+//
+//  Created by Oleksandr Bretsko on 2/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
